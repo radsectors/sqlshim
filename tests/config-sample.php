b5b6559fe99990658062900d4c95da682952755a
Marc A Hester <marc@radsectors.com>
2015-05-20 23:11:05 -0500

    tweaks to test bootstrap

--- tests/config-sample.php
+++ tests/config-sample.php
@@ -1,7 +1,7 @@
 <?php
 
 // http://sqlblog.com/blogs/jamie_thomson/archive/2012/04/10/northwind-now-available-on-sql-azure.aspx
-define('HOSTNAME', "mhknbn2kdz.database.windows.net"); // host[,port]
+define('HOSTNAME', "mhknbn2kdz.database.windows.net"); // hostname[,port]
 define('DATABASE', "AdventureWorks2012"); // database
 define('USERNAME', "sqlfamily"); // username
 define('PASSWORD', "sqlf@m1ly"); // password
