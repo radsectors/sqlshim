c6ae3ab774c7068232acc3fb36b6f356585e2d40
Christopher Dingli <cldingli@gmail.com>
2017-04-03 11:00:35 +1000

    Update sqlshim.php

--- src/sqlshim.php
+++ src/sqlshim.php
@@ -293,7 +293,7 @@ final class sqlshim
             }
         }
         // always try to convert dates because
-        if (preg_match('/\d{4}-\d{2}-\d{2}.*/', $val)) {
+        if (preg_match('/^\d{4}-\d{2}-\d{2}.*/', $val)) {
             return new \DateTime($val);
         }
 
